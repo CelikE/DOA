d093b47017b039cc3799d6cb17ced254fd540868
Emil Celik <emilcelik@gmail.com>
2017-03-07 00:05:24 +0100

    -

---
+++ Lecture7_Bintree/src/BTNodeToolkit.hpp
@@ -0,0 +1,60 @@
+
+#pragma once
+
+#include <iostream>
+#include "BTNode.hpp"
+
+template<class T>
+class BTNodeToolkit {
+public:
+	static void insert(BTNode<T>*& root, const T& data) {
+		if (root == NULL) {
+			root = new BTNode<T>(data);
+		} else if (data <= root->data) {
+			if (root->left == NULL) {
+				root->left = new BTNode<T>(data);
+				root->left->parent = root;
+			} else {
+				insert(root->left, data);
+			}
+		} else {
+			if (root->right == NULL) {
+				root->right = new BTNode<T>(data);
+				root->right->parent = root;
+			} else {
+				insert(root->right, data);
+			}
+		}
+	}
+
+	static bool search(BTNode<T>*& root, const T& data) {
+		if (root == NULL) {
+			return false;
+		} else if (root->data == data) {
+			return true;
+		} else if (root->data < data) {
+			return search(root->right, data);
+		} else {
+			return search(root->left, data);
+		}
+
+	}
+
+	static bool remove(BTNode<T>*& root, T data) {
+
+	}
+
+	static T sum(BTNode<T>* root) {
+		if (root == NULL)
+			return 0;
+		return root->data + sum(root->left) + sum(root->right);
+	}
+
+	static void print(BTNode<T>* root, int depth = 0) {
+		if (root == NULL)
+			return;
+		print(root->right, depth + 1);
+		std::cout << std::string(depth * 3, ' ') << root->data << "\n";
+		print(root->left, depth + 1);
+	}
+};
---
+++ Lecture7_Bintree/src/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+#include "BTNode.hpp"
+#include "BTNodeToolkit.hpp"
+
+int main(void) {
+	BTNode<int>* root = NULL;
+	BTNodeToolkit<int>::insert(root, 5);
+	BTNodeToolkit<int>::insert(root, 6);
+	BTNodeToolkit<int>::insert(root, 7);
+	BTNodeToolkit<int>::insert(root, 8);
+	BTNodeToolkit<int>::insert(root, 0);
+	BTNodeToolkit<int>::insert(root, 6);
+	BTNodeToolkit<int>::insert(root, 6);
+	BTNodeToolkit<int>::insert(root, 7);
+	std::cout << "Sum: " << BTNodeToolkit<int>::sum(root) << "\n";
+	BTNodeToolkit<int>::print(root);
+	std::cout << "Search 4: " << BTNodeToolkit<int>::search(root, 4) << "\n";
+	std::cout << "Search 7: " << BTNodeToolkit<int>::search(root, 7) << "\n";
+}
